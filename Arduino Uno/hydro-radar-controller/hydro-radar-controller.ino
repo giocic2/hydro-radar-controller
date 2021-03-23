63f5cce35b209d7ab1f22cc149fcf25a4c76d0a0
giocic2 <74670040+giocic2@users.noreply.github.com>
2021-03-23 18:09:24 +0100

    Minor changes during debug.

--- Arduino Uno/hydro-radar-controller/hydro-radar-controller.ino
+++ Arduino Uno/hydro-radar-controller/hydro-radar-controller.ino
@@ -8,8 +8,8 @@ void trigger();
 // SPI pinout
 const int loadEnablePin = 8;
 // Data is transferred from the shift register to one of eight latches on the rising edge of loadEnablePin.
-const int chipEnablePin = 9;
-const int chipSelectNegPin = 10; // must be set as OUTPUT, also if not used.
+const int chipEnablePin = 9; // Power-on ADF4158
+const int chipSelectNegPin = 10; // Must be set as OUTPUT, also if not used.
 const int clockPin = 13;
 const int serialOutPin = 11;
 
@@ -43,13 +43,12 @@ const byte R0[] =       { 0x80, 0x24, 0xB8, 0x00};
 const byte R1[] =       { 0x00, 0x00, 0x00, 0x01};
 const byte R2[] =       { 0x02, 0x10, 0xFF, 0xFA};
 const byte R3[] =       { 0x00, 0x00, 0x00, 0x43};
-const byte R4[] =       { 0x00, 0x1F, 0xFF, 0x84};
-const byte R5_load1[] = { 0x00, 0x0B, 0xFF, 0xFD};
-const byte R5_load2[] = { 0x00, 0x4B, 0xFF, 0xFD};
-//const byte R5_load2[] = { 0x00, 0x80, 0x00, 0x7D};
+const byte R4[] =       { 0x00, 0x1F, 0xFF, 0x84}; // 0.8s per step.
+//const byte R4[] =       { 0x00, 0x18, 0xF4, 0x04}; // 0.1s per step.
+const byte R5_load1[] = { 0x00, 0x4B, 0xFF, 0xFD};
+const byte R5_load2[] = { 0x00, 0x80, 0x00, 0x7D};
 const byte R6_load1[] = { 0x00, 0x00, 0x00, 0x56};
-const byte R6_load2[] = { 0x00, 0x40, 0x00, 0x56};
-//const byte R6_load2[] = { 0x00, 0x80, 0x00, 0x06};
+const byte R6_load2[] = { 0x00, 0x80, 0x00, 0x06};
 byte R7[] =             { 0x00, 0x02, 0xFF, 0xFF};
 
 void setup() {
@@ -86,12 +85,12 @@ void setup() {
   Serial.println("ADF4158 programming started...");
   SPI.beginTransaction(SPISettings(125000,MSBFIRST,SPI_MODE0)); // min. 125kHz; strange signals on clockPin line due to this line.
   
-  trigger();
+//  trigger();
   
   digitalWrite(chipEnablePin,LOW);
   delayMicroseconds(20);
   digitalWrite(chipEnablePin,HIGH);
-  delayMicroseconds(20);
+  delay(200); // Waiting time for ADF4158 power-on.
 
   transferRegister(R7);
   transferRegister(R6_load1);
@@ -104,9 +103,6 @@ void setup() {
   transferRegister(R1);
   transferRegister(R0); //last one to be loaded (double-buffered).
 
-//  delayMicroseconds(20);
-//  digitalWrite(chipEnablePin,LOW);
-//  delayMicroseconds(20);
   SPI.endTransaction();
 
   Serial.println("ADF4158 programming ended.");
@@ -119,6 +115,7 @@ void loop() {
 }
 
 void transferRegister(const byte registerName[]){
+  trigger();
   // Data transfer into shift register:
   for (int index=0; index<BYTES_PER_REGISTER; index++){
     SPI.transfer(registerName[index]);
@@ -128,6 +125,7 @@ void transferRegister(const byte registerName[]){
   digitalWrite(loadEnablePin,HIGH);
   delayMicroseconds(100);
   digitalWrite(loadEnablePin,LOW);
+  delay(80); // On EVB programming is 80ms.
   return;
 }
 
